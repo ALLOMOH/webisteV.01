c6f20945bfb23b6fa2f014f90f52e2ca68656cab
ALLOMOH <allomohangealex@gmail.com>
2025-08-12 15:26:44 +0000

    chore: update dependencies in package.json
    - Removed duplicate \astro\ dependency and ensured it's listed only once.
    - Added \@astrojs/vercel\ dependency.
    - Rearranged \devDependencies\ for clarity.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel';
 import tailwindcss from "@tailwindcss/vite";
 import react from '@astrojs/react';
 // https://astro.build/config
@@ -16,4 +17,9 @@ export default defineConfig({
             tailwindcss(),
         ],
     },
+    adapter: vercel({
+        webAnalytics: {
+        enabled: true,
+        },
+    }),
 });
