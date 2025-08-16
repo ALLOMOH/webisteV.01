4de12a02df123b37abb13220e74832a060d94525
ALLOMOH <allomohangealex@gmail.com>
2025-08-16 10:48:01 +0000

    # Changes to be committed: #	modified:   astro.config.mjs #	modified:   package-lock.json #	modified:   package.json #	new file:   public/robots.txt #	new file:   public/robots.txt.ts #	modified:   src/layouts/Layout.astro #

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,13 @@ import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
 import tailwindcss from "@tailwindcss/vite";
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
     output:'server',
-    integrations:[react()],
+    site:'https://it4acom.vercel.app/',
+    integrations:[react(),sitemap()],
       vite:{
         define:{
             'ssr':{
